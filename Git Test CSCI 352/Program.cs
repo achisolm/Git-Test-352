ed64542ae60a544128504e0c07d0664a23490332
adachis <adachis>
2018-09-04 10:43:27 -0500

    Program is now a Hello World

--- Git Test CSCI 352/Program.cs
+++ Git Test CSCI 352/Program.cs
@@ -10,6 +10,8 @@ namespace Git_Test_CSCI_352
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World");
+            Console.ReadKey();
         }
     }
 }
