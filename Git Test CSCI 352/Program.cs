3546c69114d691e8c1721318a422608150e9bef7
adachis <adachis>
2018-09-04 10:53:42 -0500

    Changed to simple number guess game

--- Git Test CSCI 352/Program.cs
+++ Git Test CSCI 352/Program.cs
@@ -10,7 +10,27 @@ namespace Git_Test_CSCI_352
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Random r = new Random();
+            int myNum = r.Next(1, 100);
+            int yourNum = -1;
+            int guesses = 0;
+            Console.WriteLine("I'm thinking of a number between 1 and 100:");
+            Console.WriteLine("For debug: myNum = " + myNum);
+
+            while (yourNum != myNum)
+            {
+                Console.WriteLine("Guess a number: ");
+                yourNum = Convert.ToInt32(Console.ReadLine());
+                guesses += 1;
+
+                if (yourNum < myNum)
+                {
+                    Console.WriteLine("Too low");
+                }
+                else if (yourNum > myNum)
+                    Console.WriteLine("Too high");
+            }
+            Console.WriteLine("Guessed in " + guesses);
             Console.ReadKey();
         }
     }
